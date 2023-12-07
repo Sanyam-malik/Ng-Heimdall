41f9a1beea2e5a0c66c600030c8a536c2211bf79
Sam Arora <samarora1051@gmail.com>
2023-12-08 01:08:51 +0530

    MISC: Updated DataTables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SettingsComponent } from './settings/settings.component';
+import { TagsComponent } from './tags/tags.component';
+import { ApplicationsComponent } from './applications/applications.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: DashboardComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent 
+  },
+  {
+    path: 'tags',
+    component: TagsComponent
+  }, 
+  {
+    path: 'applications',
+    component: ApplicationsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
